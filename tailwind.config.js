d7feac22644f7c13e0199677a06daf6e9101175b
jackie247 <jackieyu247@gmail.com>
2024-03-21 19:40:31 +0000

    Update tailwind.config.js
    Fix bug with colors and darkmode not working

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,21 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,mdx}'],
   darkMode: 'selector',
   theme: {
-    extend: {},
-    colors: {
-      'darkest-black': '#050505',
-      'dark-black': '#1F1F1F',
-      'lighter-black': '#2D2D2D',
-      'light-black': '#3A3A3A',
-      grey: '#757575',
-      'light-grey': '#E9E9E9',
-      'very-light-grey': '#F4F4F4',
-      white: '#FFFFFF',
-      'accent-purple': '#8F19E8',
-      'custom-purple': '#A445ED',
-      red: '#FF5252',
+    extend: {
+      colors: {
+        'darkest-black': '#050505',
+        'dark-black': '#1F1F1F',
+        'lighter-black': '#2D2D2D',
+        'light-black': '#3A3A3A',
+        grey: '#757575',
+        'light-grey': '#E9E9E9',
+        'very-light-grey': '#F4F4F4',
+        white: '#FFFFFF',
+        'accent-purple': '#8F19E8',
+        'custom-purple': '#A445ED',
+        red: '#FF5252',
+      },
     },
+
   },
 };
