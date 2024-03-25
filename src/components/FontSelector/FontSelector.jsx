e9e9be4ab18753a0c27a790147125a59724fbd72
jackie247 <jackieyu247@gmail.com>
2024-03-25 12:37:52 +0000

    Update FontSelector.jsx

--- src/components/FontSelector/FontSelector.jsx
+++ src/components/FontSelector/FontSelector.jsx
@@ -1,19 +1,20 @@
 import React, { useEffect, useState } from 'react';
 
 export default function FontSelector() {
-  const fonts = ['serif', 'sans-serif', 'mono'];
-  const [font, setFont] = useState('serif');
+  const fonts = ['mono', 'serif', 'sans-serif'];
+  const [font, setFont] = useState('mono');
   // const [oldFont, setOldFont] = useState('serif');
 
   const handleFontChange = (e) => {
-    console.log('isCalled');
     setFont(e.target.value);
   };
 
   const updateFont = () => {
     const root = document.getElementById('root');
     fonts.forEach((font) => root.classList.remove(font));
-    root.classList.add(font);
+    if (font) {
+      root.classList.add(font);
+    }
   };
 
   useEffect(() => {
@@ -21,13 +22,13 @@ export default function FontSelector() {
   }, [font]);
 
   return (
-    <div className="w-16">
+    <div className="w-16 sm:w-24">
       <select
         className="dark:text-white bg-inherit border-none w-full"
         onChange={handleFontChange}
       >
         {fonts.map((font) => (
-          <option key={font} value={font}>
+          <option className="dark:bg-dark-black" key={font} value={font}>
             {font.charAt(0).toUpperCase() + font.slice(1)}
           </option>
         ))}
