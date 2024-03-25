4d31700601a4f095ed4e22ec5b899af1afcfe9e9
jackie247 <jackieyu247@gmail.com>
2024-03-25 14:05:10 +0000

    Update AudioPlayer.jsx
    Now correctly updates after searching a new word

--- src/components/AudioPlayer/AudioPlayer.jsx
+++ src/components/AudioPlayer/AudioPlayer.jsx
@@ -2,24 +2,17 @@ import React, { useRef } from 'react';
 
 export default function AudioPlayer({ entryData }) {
   const audioObj = entryData?.phonetics.find((obj) => obj.audio.includes('https'));
-  console.log(audioObj.audio);
-
-  const audioRef = useRef(null);
 
   const playAudio = () => {
-    if (audioRef.current) {
-      audioRef.current.play();
+    if (audioObj?.audio) {
+      const audio = new Audio(audioObj.audio);
+      audio.play().catch((error) => console.error('Error playing the audio', error));
     }
   };
 
   return (
     <div id="audio-btn" className="w-12 h-12">
-      {audioObj && (
-        <audio ref={audioRef}>
-          <source src={audioObj.audio} type="audio/mpeg" />
-        </audio>
-      )}
-      <button type="button" onClick={playAudio}>
+      <button type="button" onClick={playAudio} aria-label="Listen to pronounciation audio">
         <svg xmlns="http://www.w3.org/2000/svg" className="w-full h-full" viewBox="0 0 75 75">
           <g fill="#A445ED" fillRule="evenodd">
             <circle cx="37.5" cy="37.5" r="37.5" opacity=".25" />
